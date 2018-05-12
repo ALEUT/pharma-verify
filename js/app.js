25ef62b323e1a53c476b9c7816e3d31d47ff8485
ALEUT <ialeut@gmail.com>
2018-05-12 18:06:19 +0300

    initial ui

--- js/app.js
+++ js/app.js
@@ -2,5 +2,7 @@ window.addEventListener("load", function() {
     var isExtensionExist = typeof(webExtensionWallet) !== "undefined";
     if(!isExtensionExist) {
         $('#extensionAlert').show();
+        $('#register').prop('disabled', true);
+        $('#check').prop('disabled', true);
     }
 });
